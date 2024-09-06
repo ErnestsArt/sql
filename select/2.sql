7a567fc43294d5f29c2d95d385ed3f7053086803
ErnestsArtihovics <you@example.com>
2024-09-06 09:23:24 +0300

    select for certain fields, custom fields + distinct

---
+++ select/2.sql
@@ -0,0 +1,11 @@
+-- uzdevums: atlasīt datus no tabulas customers
+-- datubāzē sql_store
+-- atlasīt vārdu, uzvārdu un punktus
+use sql_store;
+-- select 
+-- 	firs_name, 
+-- 	last_name, 
+-- 	points+10 as "points increased" 
+-- from customers;
+-- select * from customers
+select state from customers;
