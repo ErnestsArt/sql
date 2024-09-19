d6740c4ef007f15f38a4e05023b111e291307402
ErnestsArtihovics <you@example.com>
2024-09-19 09:06:25 +0300

    join excersises

--- 2selectjoin/5old.sql
+++ 2selectjoin/5old.sql
@@ -5,15 +5,24 @@ SELECT
     c.customer_id,
     o.order_id,
     o.order_date,
-    sh.name as 'shipper name',
-    os.name as 'status name'
+    sh.name AS 'shipper name',
+    os.name AS 'status name'
 FROM
     customers c
-    
         JOIN
     orders o USING (customer_id)
         JOIN
     shippers sh USING (shipper_id)
-		join
-	order_statuses os on o.status = os.order_status_id
+        JOIN
+    order_statuses os ON o.status = os.order_status_id;
+    
+    use sql_hr;
+    
+    select  
+		e.first_name,
+        e.last_name,
+        m.first_name as "manager name"
+    from employees e
+    join employees m
+    on e.reports_to = m.employee_id;
     
